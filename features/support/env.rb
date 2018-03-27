45f4e19c023632a2413d840694019204f01e4565
Harsh Agarwal <Harsh.Agarwal.ext@bbc.co.uk>
2018-03-27 16:37:31 +0530

    Adding support for Hive

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,25 +8,20 @@ require 'pry'
 
 
 def appium_caps
-  {
-    'Nexus5'=> { platformName: "android", deviceName: "Nexus 5", versionNumber: "5.1", browserName: "Chrome"},
-    'SGS 6'=> { platformName: "android", deviceName: "02157df23bc41912", versionNumber: "5.1.1", browserName: "chrome"}
-  }
+    {
+      'Nexus 5'=> { platformName: "android", deviceName: "Nexus 5", versionNumber: "6.0", browserName: "Chrome" },
+      'SGS 6'=> { platformName: "android", deviceName: "02157df23bc41912", versionNumber: "5.1.1", browserName: "Chrome" },
+      'Hive CI' => { platformName: 'Android', deviceName: ENV['ADB_DEVICE_ARG'], udid: ENV['ADB_DEVICE_ARG'], automationName: 'Appium', browserName: ENV['BROWSER'] }
+    }
 end
 
-
-Capybara.register_driver :appium do |app|
-  puts "in it"
-  caps = appium_caps.fetch('SGS 6')
-  puts "#{caps}"
-  desired_caps = caps
-  url = "http://127.0.0.1:4723/wd/hub/" # Url to your running appium server
+Capybara.register_driver current_driver do |app|
+  caps = ENV['HIVE'] == 'true' ? appium_caps.fetch('Hive CI') : appium_caps.fetch('Nexus 5')
+  puts caps.to_s
+  url = "http://localhost:#{appium_port}/wd/hub/" # Url to your running appium server
   appium_lib_options = { server_url: url }
-  all_options = { appium_lib:  appium_lib_options, caps: desired_caps }
+  all_options = { appium_lib:  appium_lib_options, caps: caps }
   Appium::Capybara::Driver.new app, all_options
 end
 
 Capybara.default_driver = :appium
-
-
-
